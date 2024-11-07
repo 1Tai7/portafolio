0c5a7d6a31ba489a901114be0e1c57ed334697c9
Ilce <ilsemilena1706@gmail.com>
2024-11-07 01:06:57 -0500

    init project

---
+++ js/main.js
@@ -0,0 +1,83 @@
+$(window).load(function () {
+  $("#preloader").fadeOut("slow", function () {
+    $(this).remove();
+  });
+});
+
+/******************************************************************************************************************************
+Learn More Page Scroll
+*******************************************************************************************************************************/
+$(function () {
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
+});
+
+/******************************************************************************************************************************
+Menu
+*******************************************************************************************************************************/
+(function () {
+  var bodyEl = document.body,
+    //content = document.querySelector( '.content-wrap' ),
+    openbtn = document.getElementById("open-button"),
+    closebtn = document.getElementById("close-button"),
+    isOpen = false;
+
+  function init() {
+    initEvents();
+  }
+
+  function initEvents() {
+    openbtn.addEventListener("click", toggleMenu);
+    if (closebtn) {
+      closebtn.addEventListener("click", toggleMenu);
+    }
+
+    /* close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+		*/
+  }
+
+  function toggleMenu() {
+    if (isOpen) {
+      classie.remove(bodyEl, "show-menu");
+    } else {
+      classie.add(bodyEl, "show-menu");
+    }
+    isOpen = !isOpen;
+  }
+
+  init();
+})();
+
+function enviarCorreo(event) {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+  if (email != null) {
+    var sLink =
+      "mailto:ilsemilena1706@gmail.com" +
+      "?subject=" +
+      subject +
+      "&body=" +
+      `	correo: ${email} 
+	  	mesaje: ${message}`;
+    window.location.href = sLink;
+  }
+}
